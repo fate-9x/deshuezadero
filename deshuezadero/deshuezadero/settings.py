b8a390e1d3ae6ef8a381cebd025a942fd243a46f
Cote17 <cote1704@gmail.com>
2023-05-15 18:14:06 -0400

    Template

--- deshuezadero/deshuezadero/settings.py
+++ deshuezadero/deshuezadero/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'deshuezadero.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'deshuesadero_pruebas',
+        'NAME': 'deshuesadero_prueba',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
